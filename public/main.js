ec47adfc9592fc53af48ddf91519266773b7e20e
Kelly Miller <k.macleod21@gmail.com>
2016-12-07 01:25:21 +0000

    basic drawing board features complete and working

--- public/main.js
+++ public/main.js
@@ -1,19 +1,19 @@
 //allows drawing to the canvas
 var pictionary = function() {
     var canvas, context;
-    
+//tells context that new object will start being drawn    
     var draw = function(position) {
         context.beginPath();
         context.arc(position.x, position.y, 6, 0, 2 * Math.PI);
         context.fill();
     };
-    
+//selects the canvas element and allows user to create a drawing context    
     canvas = $('canvas');
     context = canvas[0].getContext('2d');
     canvas[0].width = canvas[0].offsetWidth;
     canvas[0].height = canvas[0].offsetHeight;
     canvas.on('mousemove', function(event) {
-        var offset = canvas.offser();
+        var offset = canvas.offset();
         var position = {x: event.pageX - offset.left,
                         y: event.pageY - offset.top};
         draw(position);
