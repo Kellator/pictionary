f1dab733bc1b17857647c8b5938bca278d6c82c8
Kelly Miller <k.macleod21@gmail.com>
2016-12-07 01:09:43 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+//boilerplate code to serve static assets and create the Socket.IO engine
+var http = require('http');
+var express = require('express');
+var socket_io = require('socket.io');
+
+var app = express();
+app.use(express.static('public'));
+
+var server = http.Server(app);
+var io = socket_io(server);
+
+server.listen(process.env.PORT || 8080);
